e041ef50ac58515aeb32ecf70f8d07ca0ac6d1bc
Vitachan <165843769+irfadillah30@users.noreply.github.com>
2025-03-16 23:21:49 +0900

    up

--- kontak.py
+++ kontak.py
@@ -3,18 +3,6 @@ import streamlit as st
 def munculkan_kontak():
     st.title("Kontak")
     st.write("Hubungi saya melalui tautan berikut:")
-
-    # LinkedIn
-    st.markdown(
-        "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue)](https://www.linkedin.com/in/irfadillah-afni-nurvita/)"
-    )
-
-    # WhatsApp
-    st.markdown(
-        "[![WhatsApp](https://img.shields.io/badge/WhatsApp-Profile-brightgreen)](https://wa.me/6282123970898)"
-    )
-
-    # Email
-    st.write("📧 Email: irfadillahafninurvita@gmail.com")
-
-
+    st.write("Email:[irfadillahafninurvita@gmail.com](irfadillahafninurvita@gmail.com)")
+    st.write("LinkedIn:[Irfadillah Afni Nurvita](https://www.linkedin.com/in/irfadillah-afni-nurvita/)")
+    st.write("Whatsapp:[+6289530000000](https://wa.me/6289530000000)")
